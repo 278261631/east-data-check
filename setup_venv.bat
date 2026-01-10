c646c5e486408dd72d5a8abdd506c9e39ebc1b83
278261631 <mars.maya.g@gmail.com>
2026-01-10 19:21:46 +0800

    虚拟环境

--- setup_venv.bat
+++ setup_venv.bat
@@ -1,9 +1,9 @@
 @echo off
 echo Creating virtual environment...
-C:\Python\Python310\python.exe -m venv venv
+python.exe -m venv venv
 echo Installing dependencies...
 call venv\Scripts\activate.bat
-pip install django python-decouple django-cors-headers openpyxl
+python -m pip install django python-decouple django-cors-headers openpyxl -i https://pypi.tuna.tsinghua.edu.cn/simple
 echo Done! Run start.bat to start the server.
 pause
 
