3cdf7255877d697f8d4b821e56f93cac20845102
278261631 <mars.maya.g@gmail.com>
2026-01-10 18:56:02 +0800

    Change admin password and port to 28000

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
 @echo off
 C:\Python\Python310\python.exe manage.py migrate --run-syncdb
 C:\Python\Python310\python.exe manage.py initadmin
-C:\Python\Python310\python.exe manage.py runserver 0.0.0.0:8000
+C:\Python\Python310\python.exe manage.py runserver 0.0.0.0:28000
 
