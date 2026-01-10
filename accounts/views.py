92f6b6cdd7bfa70bdb98ea446365a9ad0260b765
278261631 <mars.maya.g@gmail.com>
2026-01-10 15:43:38 +0800

    Disable registration, admin only creates users

--- accounts/views.py
+++ accounts/views.py
@@ -2,22 +2,6 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import messages
-from .forms import RegisterForm
-
-
-def register_view(request):
-    if request.user.is_authenticated:
-        return redirect('home')
-    if request.method == 'POST':
-        form = RegisterForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            messages.success(request, 'Registration successful!')
-            return redirect('home')
-    else:
-        form = RegisterForm()
-    return render(request, 'accounts/register.html', {'form': form})
 
 
 def login_view(request):
